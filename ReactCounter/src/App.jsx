d97bcda0b4cf820dc456906198805af4aa381b47
Akash Mehra <mehraaksh2812@gmail.com>
2023-10-21 19:53:38 +0530

    Changes in ReactRouter

--- ReactCounter/src/App.jsx
+++ ReactCounter/src/App.jsx
@@ -1,46 +1,46 @@
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 // import './App.css'
 
 function App() {
-  
+
   const [counter, setCounter] = useState(15)
   const addValue = () => {
-    if(counter <= 19){
+    if (counter <= 19) {
       setCounter(counter + 1)
     }
-    else{
-      toast.warn("Sorry You can not exceed the Highest Value "),{
-        theme: "colored"
+    else {
+      toast.warn("Sorry You can not exceed the Highest Value "), {
+        theme:"dark"
       }
     }
   }
-  const removeValue = () =>{
-    if(counter >= 1){
+  const removeValue = () => {
+    if (counter >= 1) {
       setCounter(counter - 1)
     }
-    else{
-      toast.warn(" You can not exceed the Minimum value"),{
-        theme: "colored"
+    else {
+      toast.error(" You can not exceed the Minimum value"), {
+        theme: "colored",
       }
     }
   }
   return (
     <>
-    <div className='main-container'>
-    <ToastContainer />
-    <div className="container">
-        <h1> React Counter :{counter}</h1>
+      <ToastContainer
+      theme="colored" />
+      <div className='main-container'>
+        <div className="container">
+          <h1> React Counter :{counter}</h1>
+        </div>
+        <div className='btn-container'>
+          <button onClick={addValue}>Add Value </button>
+          <button onClick={removeValue}>Remove Value </button>
+        </div>
       </div>
-      <div className='btn-container'>
-        <button onClick={addValue}>Add Value </button>
-        <button onClick={removeValue}>Remove Value </button>
-      </div>
-    </div>
     </>
   )
 }
